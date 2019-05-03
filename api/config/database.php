5c44866ff537b262bb86ab8a94cccc080e4664f3
Ville Kerri <ville.kerri@hotmail.com>
2019-05-03 14:04:17 +0300

    database connection

--- api/config/database.php
+++ api/config/database.php
@@ -1,17 +1,32 @@
 <?php
 class Database{
     private $host = "localhost";
-    private $db_name = "webstoredatabase";
+    private $db_name = "webstore";
     private $username = "root";
     private $password = "root";
     public $conn;
 
     public function getConnection(){
+        try {
+            $conn = new PDO("mysql:host=localhost;dbname=webstore", "root", "root");
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo "Connection failed: " . $e->getMessage();
+        }
+        /*
         $this->conn = new mysqli($this->db_name, $this->username, $this->password);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
+        */
         return $this->conn;
     }
+
+
+
 }
 ?>
