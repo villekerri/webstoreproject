05f9e3cb164eb04ec1b2a6cea87bc4c60568d469
Heidi Joensuu <cedi@hotmail.fi>
2019-05-08 10:38:36 +0300

    reformed unfinish login

---
+++ js/jquery/login.js
@@ -0,0 +1,188 @@
+// log in and register
+$(document).ready(function(){
+    // show sign up / registration form
+    $(document).on('click', '#logReg', function(){
+
+        var html = `
+
+
+
+            <h2>Sign Up</h2>
+            <form id='sign_up_form'>
+                <div class="form-group">
+                    <label for="firstname">Firstname</label>
+                    <input type="text" class="form-control" name="firstname" id="firstname" required />
+                </div>
+
+                <div class="form-group">
+                    <label for="lastname">Lastname</label>
+                    <input type="text" class="form-control" name="lastname" id="lastname" required />
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" id="email" required />
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" required />
+                </div>
+
+                <button type='submit' class='btn btn-primary'>Sign Up</button>
+            </form>
+            `;
+
+
+
+
+        clearResponse();
+        $('#products').html(html);
+    });
+
+    // trigger when registration form is submitted
+    $(document).on('submit', '#sign_up_form', function(){
+
+        // get form data
+        var sign_up_form=$(this);
+        var form_data=JSON.stringify(sign_up_form.serializeObject());
+
+        // submit form data to api
+        $.ajax({
+            url: "api/create_user.php",
+            type : "POST",
+            contentType : 'application/json',
+            data : form_data,
+            success : function(result) {
+                // if response is a success, tell the user it was a successful sign up & empty the input boxes
+                $('#response').html("<div class='alert alert-success'>Successful sign up. Please login.</div>");
+                sign_up_form.find('input').val('');
+            },
+            error: function(xhr, resp, text){
+                // on error, tell the user sign up failed
+                $('#response').html("<div class='alert alert-danger'>Unable to sign up. Please contact admin.</div>");
+            }
+        });
+
+        return false;
+    });
+
+
+
+
+
+
+/*
+
+    // show login form
+    $(document).on('click', '#login', function(){
+        showLoginPage();
+    });
+
+// trigger when login form is submitted
+    $(document).on('submit', '#login_form', function(){
+
+        // get form data
+        var login_form=$(this);
+        var form_data=JSON.stringify(login_form.serializeObject());
+
+        // submit form data to api
+        $.ajax({
+            url: "api/login.php",
+            type : "POST",
+            contentType : 'application/json',
+            data : form_data,
+            success : function(result){
+
+                // store jwt to cookie
+                setCookie("jwt", result.jwt, 1);
+
+                // show home page & tell the user it was a successful login
+                showHomePage();
+                $('#response').html("<div class='alert alert-success'>Successful login.</div>");
+
+            },
+            error: function(xhr, resp, text){
+                // on error, tell the user login has failed & empty the input boxes
+                $('#response').html("<div class='alert alert-danger'>Login failed. Email or password is incorrect.</div>");
+                login_form.find('input').val('');
+            }
+        });
+
+        return false;
+    });
+
+
+*/
+    // remove any prompt messages
+    function clearResponse(){
+        $('#response').html('');
+    }
+/*
+// show login page
+    function showLoginPage(){
+
+        // remove jwt
+        setCookie("jwt", "", 1);
+
+        // login page html
+        var html = `
+        <h2>Login</h2>
+        <form id='login_form'>
+            <div class='form-group'>
+                <label for='email'>Email address</label>
+                <input type='email' class='form-control' id='email' name='email' placeholder='Enter email'>
+            </div>
+
+            <div class='form-group'>
+                <label for='password'>Password</label>
+                <input type='password' class='form-control' id='password' name='password' placeholder='Password'>
+            </div>
+
+            <button type='submit' class='btn btn-primary'>Login</button>
+        </form>
+        `;
+
+        $('#content').html(html);
+        clearResponse();
+        showLoggedOutMenu();
+    }
+
+// function to set cookie
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+// if the user is logged out
+    function showLoggedOutMenu(){
+        // show login and sign up from navbar & hide logout button
+        $("#login, #sign_up").show();
+        $("#logout").hide();
+    }
+
+// showHomePage() function will be here
+
+// function to make form values to json format
+    $.fn.serializeObject = function(){
+
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
+*/
+});
+
