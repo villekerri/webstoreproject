f2b85329905c88150997f7af81958728441c51b0
Antti Alho <antti.s.alho@gmail.com>
2019-05-09 22:08:33 +0300

    cleanup

--- js/jquery/order.js
+++ js/jquery/order.js
@@ -1,15 +1,8 @@
-//orders
 $(document).ready(function(){
-    //http://192.168.33.10/api/order/read.php
     $(document).on('click', '#ordersbutton', async function(){
-        console.log("printing user id");
-        var a = await getUserId();
-        console.log(a);
-        console.log("user id printed");
-        var orders = await $.getJSON("http://192.168.33.10/api/order/read.php", function(data){
-            console.log(data);
-        });
-        console.log(orders);
+
+        var orders = await $.getJSON("http://192.168.33.10/api/order/read.php", function(data){});
+        
         var tassa = function () {
             var jotain = "<table><tr><th>Order ID</th><th>Status</th><th>Productorder ID</th><th>Product</th><th>Quantity</th></tr>";
             for (var i = 0; i < orders.orders_list.length ; i++){
@@ -21,15 +14,14 @@ $(document).ready(function(){
             jotain += "</table>"
             return jotain
         }
+        
         var html = `<h2>List of the orders</h2>` + tassa();
         clearResponse();
         $('#home').html(html);
     });
 
-    
-
-
     function clearResponse(){
         $('#response').html('');
     }
+
 });
